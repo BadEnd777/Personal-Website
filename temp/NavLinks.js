6924bc8dd8e24f7befd82d855ac45a7aa06e56df
BadEnd777 <badend23@hotmail.com>
2023-09-08 14:48:08 +0700

    Refactor: Rename src/data to src/data_temp

---
+++ temp/NavLinks.js
@@ -0,0 +1,7 @@
+const NavLinks = [
+    { name: 'Home', href: '/' },
+    { name: 'Projects', href: '/projects' },
+    { name: 'Blog', href: '/blog' }
+]
+
+export default NavLinks
---
+++ temp/Projects.js
@@ -0,0 +1,34 @@
+const Projects = [
+    {
+        name: 'Chat Bridge',
+        description: 'A library that help you to create chatbots for Facebook Messenger.',
+        link: 'https://www.npmjs.com/package/@badend/chatbridge',
+        techs: ['Fastify', 'Undici'],
+        image: '/images/projects/chat-bridge.jpeg'
+    },
+    {
+        name: 'Vacant SDK',
+        description:
+            'A website for Vacant SDK, a software development kit for creating Avatars for the VRChat platform.',
+        link: 'https://vacant-sdk.pages.dev/',
+        techs: ['SvelteKit', 'Tailwind CSS', 'Svelte Icons Pack'],
+        image: '/images/projects/vacant-sdk.jpeg'
+    },
+    {
+        name: 'Kafka Main',
+        description:
+            'A website for Kafka Main, a character building guidelines for the game Honkai Star Rail.',
+        link: 'https://kafka-main.pages.dev/',
+        techs: ['Next.js', 'Chakra UI', 'React Icons', 'Catppuccino Palette'],
+        image: '/images/projects/kafka-main.jpeg'
+    },
+    {
+        name: 'Portfolio',
+        description: 'My portfolio website.',
+        link: 'https://github.com/BadEnd777/Portfolio-Website',
+        techs: ['Next.js', 'Chakra UI', 'React Icons', 'React Simple Typewriter'],
+        image: '/images/projects/portfolio.jpeg'
+    }
+]
+
+export default Projects
