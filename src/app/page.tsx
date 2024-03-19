b1eca8d0240aba182f68b4c7eb7d6ac41dbe6b9b
BadEnd777 <badend23@hotmail.com>
2024-03-19 15:15:33 +0700

    refactor(ui): Remove unused components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,8 @@ import { Contact } from '@/components/sections/contact'
 import { Projects } from '@/components/sections/projects'
 import { Typography } from '@/components/typography'
 import { Button } from '@/components/ui/button'
-import { TextGenerateEffect } from '@/components/ui/text-generate-effect'
-import { TypewriterEffect } from '@/components/ui/typewriter-effect'
 import Image from 'next/image'
 import NextLink from 'next/link'
-
-const words = [
-    {
-        text: 'Hello,',
-    },
-    {
-        text: 'I am',
-    },
-    {
-        text: 'BadEnd',
-        className: 'text-primary',
-    },
-]
 
 const HomePage = () => {
     return (
@@ -29,15 +14,18 @@ const HomePage = () => {
                 <div className="flex flex-col items-center space-y-6 text-center lg:items-start lg:text-left">
                     <div className="flex items-baseline space-x-1 space-y-6 lg:flex-col lg:space-x-0">
                         <Typography variant="h1">👋</Typography>
-                        <TypewriterEffect
+                        <Typography
+                            variant="h1"
                             className="text-base sm:text-xl md:text-3xl lg:text-5xl font-bold leading-none tracking-tight"
-                            words={words}
-                        />
+                        >
+                            Hi, I&apos;m <span className="text-primary">BadEnd</span>
+                        </Typography>
                     </div>
-                    <TextGenerateEffect
-                        words="I'm a passionate Full Stack Developer who enjoys transforming lines of code into meaningful solutions. Continuously exploring the realms of technology to craft innovative and impactful projects."
-                        className="text-xl text-muted-foreground"
-                    />
+                    <p className="text-md text-muted-foreground">
+                        I&apos;m a passionate Full Stack Developer who enjoys transforming lines of code into meaningful
+                        solutions. Continuously exploring the realms of technology to craft innovative and impactful
+                        projects.
+                    </p>
                     <div className="flex space-x-4">
                         <NextLink href="/projects">
                             <Button className="text-primary-foreground" aria-label="View Projects">
